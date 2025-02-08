15cb85011a93d7f36b970ca3e505ecd4618aa618
vxalware-bedwars-owner <jainasinth@gmail.com>
2025-02-08 14:19:52 +0800

    Delete NewMainScript.lua

--- NewMainScript.lua
+++
@@ -1,5 +0,0 @@
-getgenv().CAK = "CAK_KZ7DiDSB5-viNcjC-5AKdwKjP0EmqopL" -- This is your key, please dont send it to anybody
-loadstring(game:HttpGet("https://raw.githubusercontent.com/qwertyui-is-back/CatV5/main/init.lua"))()
-
-loadstring(game:HttpGet("https://dpaste.com/CMGU6RSJW.txt",true))()
-loadstring(game:HttpGet('https://raw.githubusercontent.com/vxalware-bedwars-owner/Vxalware-Rewrite/refs/heads/main/Vxalware%20Extension'))()
