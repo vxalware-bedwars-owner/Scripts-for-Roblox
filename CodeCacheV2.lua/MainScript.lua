1c838d1cbbbf9ecc72a74082022e4ffb231daebe
Vxalware <jainasinth@gmail.com>
2025-02-23 16:52:46 +0800

    Update MainScript.lua

--- CodeCacheV2.lua/MainScript.lua
+++ CodeCacheV2.lua/MainScript.lua
@@ -4,4 +4,54 @@ local win = DiscordLib:Window("discord library")
 
 local serv = win:Server("Local", "http://www.roblox.com/asset/?id=6031075938")
 
-serv:Channel("by dawid#7205")
+local main = serv:Channel("A Games")
+
+local main = serv:Channel("B Games")
+
+local main = serv:Channel("C Games")
+
+local main = serv:Channel("D Games")
+
+local main = serv:Channel("E Games")
+
+local main = serv:Channel("F Games")
+
+local main = serv:Channel("G Games")
+
+local main = serv:Channel("H Games")
+
+local main = serv:Channel("I Games")
+
+local main = serv:Channel("J Games")
+
+local main = serv:Channel("K Games")
+
+local main = serv:Channel("L Games")
+
+local main = serv:Channel("M Games")
+
+local main = serv:Channel("N Games")
+
+local main = serv:Channel("O Games")
+
+local main = serv:Channel("P Games")
+
+local main = serv:Channel("Q Games")
+
+local main = serv:Channel("R Games")
+
+local main = serv:Channel("S Games")
+
+local main = serv:Channel("T Games")
+
+local main = serv:Channel("U Games")
+
+local main = serv:Channel("V Games")
+
+local main = serv:Channel("W Games")
+
+local main = serv:Channel("X Games")
+
+local main = serv:Channel("Y Games")
+
+local main = serv:Channel("Z Games")
