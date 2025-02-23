9d36232287541839caf1ee587c4d258c6e8b68a8
Vxalware <jainasinth@gmail.com>
2025-02-23 17:11:18 +0800

    Update MainScript.lua

--- CodeCacheV2.lua/MainScript.lua
+++ CodeCacheV2.lua/MainScript.lua
@@ -4,58 +4,54 @@ local win = DiscordLib:Window("discord library")
 
 local serv = win:Server("Local", "http://www.roblox.com/asset/?id=6031075938")
 
-local main = serv:Channel("A Games")
+local main2 = serv:Channel("A Games")
 
-local main = serv:Channel("B Games")
+local main3 = serv:Channel("B Games")
 
-local drop = drops:Dropdown("Blox fruits script hubs",{"Redz Hub","Ronix Hub","Speed Hub X","Alchemy Hub","Kncrypt"}, function(bool)
-print(bool)
-end)
+local main4 = serv:Channel("C Games")
 
-local main = serv:Channel("C Games")
+local main5 = serv:Channel("D Games")
 
-local main = serv:Channel("D Games")
+local main6 = serv:Channel("E Games")
 
-local main = serv:Channel("E Games")
+local main7 = serv:Channel("F Games")
 
-local main = serv:Channel("F Games")
+local main8 = serv:Channel("G Games")
 
-local main = serv:Channel("G Games")
+local main9 = serv:Channel("H Games")
 
-local main = serv:Channel("H Games")
+local main10 = serv:Channel("I Games")
 
-local main = serv:Channel("I Games")
+local main11 = serv:Channel("J Games")
 
-local main = serv:Channel("J Games")
+local main12 = serv:Channel("K Games")
 
-local main = serv:Channel("K Games")
+local main13 = serv:Channel("L Games")
 
-local main = serv:Channel("L Games")
+local main14 = serv:Channel("M Games")
 
-local main = serv:Channel("M Games")
+local main15 = serv:Channel("N Games")
 
-local main = serv:Channel("N Games")
+local main16 = serv:Channel("O Games")
 
-local main = serv:Channel("O Games")
+local main17 = serv:Channel("P Games")
 
-local main = serv:Channel("P Games")
+local main18 = serv:Channel("Q Games")
 
-local main = serv:Channel("Q Games")
+local main19 = serv:Channel("R Games")
 
-local main = serv:Channel("R Games")
+local main20 = serv:Channel("S Games")
 
-local main = serv:Channel("S Games")
+local main21 = serv:Channel("T Games")
 
-local main = serv:Channel("T Games")
+local main22 = serv:Channel("U Games")
 
-local main = serv:Channel("U Games")
+local main23 = serv:Channel("V Games")
 
-local main = serv:Channel("V Games")
+local main24 = serv:Channel("W Games")
 
-local main = serv:Channel("W Games")
+local main25 = serv:Channel("X Games")
 
-local main = serv:Channel("X Games")
+local main26 = serv:Channel("Y Games")
 
-local main = serv:Channel("Y Games")
-
-local main = serv:Channel("Z Games")
+local main27 = serv:Channel("Z Games")
