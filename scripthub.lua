6eb7b94170b40c16323deb24ee62550a0cea47e6
sstvskids <staavstudios@gmail.com>
2025-03-08 12:33:19 +1100

    hide them in the gui 🔥

--- scripthub.lua
+++ scripthub.lua
@@ -1,28 +1,10 @@
 local DiscordLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/sstvskids/Scripts-for-Roblox/refs/heads/main/libs/gui.lua"))()
-local win = DiscordLib:Window("CodeCacheV2 Re-made by SynthX, A Nerd, and Vxalware")
+local win = DiscordLib:Window("CodeCacheV2 remade by stav")
 local serv = win:Server("Game Scripts", "http://www.roblox.com/asset/?id=6031075938")
 local core = {
     tabs = {
         bloxfruits = serv:Channel("Blox-Fruits"),
         bedwars = serv:Channel("Bedwars")
-    }
-}
-
-local scripts = {
-    fruitscripts = {
-        Redzhub = "https://raw.githubusercontent.com/realredz/BloxFruits/refs/heads/main/Source.lua",
-        Ronix = "https://api.luarmor.net/files/v3/loaders/b5f968ca22436160479678e830766cc4.lua",
-        Speed = "https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/Speed%20Hub%20X.lua",
-        Alchemy = "https://scripts.alchemyhub.xyz",
-        Kncrypt = "https://raw.githubusercontent.com/3345-c-a-t-s-u-s/Kncrypt/refs/heads/main/sources/BloxFruit.lua"
-    },
-    bedwarscripts = {
-        VoidPaste = "https://raw.githubusercontent.com/VapeVoidware/vapevoidware/main/NewMainScript.lua",
-        Vxalware = "https://raw.githubusercontent.com/vxalware-bedwars-owner/Vxalware-Rewrite/refs/heads/main/Vxware-Bedwars.lua",
-        AuroraGPT = "https://raw.githubusercontent.com/cocotv666/Aurora/main/Aurora_Loader",
-        VapeV4 = "https://raw.githubusercontent.com/sstvskids/VapeV4ForRoblox/main/NewMainScript.lua",
-        Alsploit = "https://dpaste.com/BQ7GD3DD2.txt",
-        Night = "https://raw.githubusercontent.com/warprbx/NightRewrite/refs/heads/main/Night/Loader.luau"
     }
 }
 
@@ -36,18 +18,18 @@ end
 
 core.tabs.bloxfruits:Dropdown(
     "Blox Fruits Script Hubs 🍎",
-    gettable(scripts.fruitscripts),
+    gettable(DiscordLib.scripts.fruitscripts),
     function(value)
-        local url = scripts.fruitscripts[value]
+        local url = DiscordLib.scripts.fruitscripts[value]
         loadstring(game:HttpGet(url))()
     end
 )
 
 core.tabs.bloxfruits:Dropdown(
     "Bedwars Scripts 🛏️",
-    gettable(scripts.bedwarscripts),
+    gettable(DiscordLib.scripts.bedwarscripts),
     function(value)
-        local url = scripts.bedwarscripts[value]
+        local url = DiscordLib.scripts.bedwarscripts[value]
         loadstring(game:HttpGet(url))()
     end
 )
