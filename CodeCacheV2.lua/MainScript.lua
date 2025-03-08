8ffd5eeeff6937e5b660ec8c071ad93ebe666a60
Vxalware <jainasinth@gmail.com>
2025-03-08 16:56:59 +0800

    Added A-Z Game Channels

--- CodeCacheV2.lua/MainScript.lua
+++ CodeCacheV2.lua/MainScript.lua
@@ -2,42 +2,54 @@ local DiscordLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/bl
 local win = DiscordLib:Window("CodeCacheV2 Re-made by SynthX, A Nerd, and Vxalware")
 local serv = win:Server("Game Scripts", "http://www.roblox.com/asset/?id=6031075938")
 
-local scripts = {
-    fruitscripts = {
-        Redzhub = loadstring(game:HttpGet("https://raw.githubusercontent.com/realredz/BloxFruits/refs/heads/main/Source.lua"))(),
-        Ronix = loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/b5f968ca22436160479678e830766cc4.lua"))(),
-        Speed = loadstring(game:HttpGet("https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/Speed%20Hub%20X.lua"))(),
-        Alchemy = loadstring(game:HttpGet("https://scripts.alchemyhub.xyz"))(),
-        Kncrypt = loadstring(game:HttpGet("https://raw.githubusercontent.com/3345-c-a-t-s-u-s/Kncrypt/refs/heads/main/sources/BloxFruit.lua"))()
-    },
-    bedwarscripts = {
-        VoidPaste = loadstring(game:HttpGet("https://raw.githubusercontent.com/VapeVoidware/vapevoidware/main/NewMainScript.lua", true))(),
-        Vxalware = loadstring(game:HttpGet("https://raw.githubusercontent.com/vxalware-bedwars-owner/Vxalware-Rewrite/refs/heads/main/Vxware-Bedwars.lua"))(),
-        AuroraGPT = loadstring(game:HttpGet("https://raw.githubusercontent.com/cocotv666/Aurora/main/Aurora_Loader"))(),
-        VapeV4 = loadstring(game:HttpGet("https://raw.githubusercontent.com/sstvskids/VapeV4ForRoblox/main/NewMainScript.lua", true))(),
-        Alsploit = loadstring(game:HttpGet("https://dpaste.com/BQ7GD3DD2.txt", true))(),
-        Night = loadstring(game:HttpGet("https://raw.githubusercontent.com/warprbx/NightRewrite/refs/heads/main/Night/Loader.luau"))()
-    }
-}
+local drops = serv:Channel("A Game Scripts")
 
-local drop =
-    drops:Dropdown("Blox Fruits Script Hubs 🍎", scripts.fruitscripts,
-    function(value)
-        for i,v in pairs(scripts.fruitscripts) do
-            if v == value then
-                loadstring(v)()
-            end
-        end
-    end
-)
+local drops = serv:Channel("B Game Scripts")
 
-local drop =
-    drops:Dropdown("Bedwars Scripts 🛏️", scripts.bedwarscripts,
-    function(value)
-        for i,v in pairs(scripts.bedwarscripts) do
-            if v == value then
-                loadstring(v)()
-            end
-        end
-    end
-)
+local drops = serv:Channel("C Game Scripts")
+
+local drops = serv:Channel("D Game Scripts")
+
+local drops = serv:Channel("E Game Scripts")
+
+local drops = serv:Channel("F Game Scripts")
+
+local drops = serv:Channel("G Game Scripts")
+
+local drops = serv:Channel("H Game Scripts")
+
+local drops = serv:Channel("I Game Scripts")
+
+local drops = serv:Channel("J Game Scripts")
+
+local drops = serv:Channel("K Game Scripts")
+
+local drops = serv:Channel("L Game Scripts")
+
+local drops = serv:Channel("M Game Scripts")
+
+local drops = serv:Channel("N Game Scripts")
+
+local drops = serv:Channel("O Game Scripts")
+
+local drops = serv:Channel("P Game Scripts")
+
+local drops = serv:Channel("Q Game Scripts")
+
+local drops = serv:Channel("R Game Scripts")
+
+local drops = serv:Channel("S Game Scripts")
+
+local drops = serv:Channel("T Game Scripts")
+
+local drops = serv:Channel("U Game Scripts")
+
+local drops = serv:Channel("V Game Scripts")
+
+local drops = serv:Channel("W Game Scripts")
+
+local drops = serv:Channel("X Game Scripts")
+
+local drops = serv:Channel("Y Game Scripts")
+
+local drops = serv:Channel("Z Game Scripts")
