1d1652f03becb01a006da11cd148540b68fdb4d9
Vxalware <jainasinth@gmail.com>
2025-03-09 20:10:55 +0800

    Fix Loader.lua

--- CodeCacheV2.lua/Loader.lua
+++ CodeCacheV2.lua/Loader.lua
@@ -1,1 +1,1 @@
-return loadstring(game:HttpGet("https://raw.githubusercontent.com/vxalware-bedwars-owner/Scripts-for-Roblox/refs/heads/main/CodeCacheV2.lua/NewMainScript.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/vxalware-bedwars-owner/Scripts-for-Roblox/refs/heads/main/CodeCacheV2.lua/NewMainScript.lua"))()
