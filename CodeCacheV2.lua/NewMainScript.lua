026de6062118a3eb4e4fc3746e2a63318f824885
Vxalware <jainasinth@gmail.com>
2025-03-09 20:08:15 +0800

    Fix

--- CodeCacheV2.lua/NewMainScript.lua
+++ CodeCacheV2.lua/NewMainScript.lua
@@ -4,7 +4,7 @@ local TweenService = game:GetService("TweenService")
 local Lighting = game:GetService("Lighting")
 
 --> CodeCache Beta <--
-loadstring(game:HttpGet("https://raw.githubusercontent.com/vxalware-bedwars-owner/Scripts-for-Roblox/refs/heads/main/CodeCacheV2.lua/MainScript.lua", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/vxalware-bedwars-owner/Scripts-for-Roblox/refs/heads/main/CodeCacheV2.lua/MainScript.lua"))()
 
 --> CodeCache Beta Variables <--
 local DiscordUI = CoreGui:FindFirstChild("Discord")
