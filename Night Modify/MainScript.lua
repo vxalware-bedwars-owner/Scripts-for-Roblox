23858c4fbbcd3c5b50f29e106f0f97e7ce448651
Vxalware <jainasinth@gmail.com>
2025-03-17 16:44:38 +0800

    Update MainScript.lua

--- Night Modify/MainScript.lua
+++ Night Modify/MainScript.lua
@@ -1,14 +1,5 @@
 -- Night Script loader
 loadstring(game:HttpGet("https://raw.githubusercontent.com/warprbx/NightRewrite/refs/heads/main/Night/Loader.luau"))()
-
--- Proper no fall damage script
---> NO FALL DAMAGE SCRIPT IS ALREADY INCLUDED IN VXALWARE EXTENSION <--
-
--- Adding Scaffold later
-
--- Adding Antivoid later
-
 -- Vxalware Extension for stuff
 loadstring(game:HttpGet("https://raw.githubusercontent.com/vxalware-bedwars-owner/Vxalware-Rewrite/refs/heads/main/Assets/Vxalware%20Extension.lua"))()
-
--- Updates to no fall damage script like reset = uninject will be fixed soon!
+-- Adding Antivoid, Scaffold, CFrame Speed bypass, and Creating seperate Extension
