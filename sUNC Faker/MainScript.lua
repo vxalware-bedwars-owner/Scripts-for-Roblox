5cbb33ea3c70f7ab647821644dd9ccb6cd4a32c5
Vxalware <jainasinth@gmail.com>
2025-03-17 17:16:23 +0800

    Delete sUNC Faker directory

--- sUNC Faker/MainScript.lua
+++
@@ -1,122 +0,0 @@
-local everything = {
-    "Loading...",
-    "Grabbing executor name...",
-    "Starting sUNC test. Join our Discord server if you want :) [discord.gg/rx4nbdQPx6]",
-    "â debug.setupvalue",
-    "â debug.setconstant",
-    "â gethiddenproperty",
-    "â sethiddenproperty",
-    "â Starting basic loadstring testing",
-    "â Finished basic loadstring testing",
-    "â Starting simple loadstring URL testing...",
-    "â Passed the simple loadstring test. Short obfuscated scripts can be executed.",
-    "â Finished simple loadstring URL testing",
-    "â Starting complicated loadstring URL testing...",
-    "â Finished complicated loadstring URL testing",
-    "loadstring[complicated] true",
-    "loadstring[simple] true",
-    "loadstring[basic] true",
-    "Interesting",
-    "â getcallbackvalue",
-    "â decompile",
-    "\n\n\n",
-    "â Drawing.Fonts",
-    "â Drawing.new",
-    "â WebSocket.connect",
-    "â appendfile",
-    "â base64_decode",
-    "â base64_encode",
-    "â cache.invalidate",
-    "â cache.iscached",
-    "â cache.replace",
-    "â checkcaller",
-    "â cleardrawcache",
-    "â clonefunction",
-    "â cloneref",
-    "â compareinstances",
-    "â crypt.decrypt",
-    "â crypt.encrypt",
-    "â crypt.generatebytes",
-    "â crypt.generatekey",
-    "â crypt.hash",
-    "â debug.getconstant",
-    "â debug.getconstants",
-    "â debug.getinfo",
-    "â debug.getproto",
-    "â debug.getprotos",
-    "â debug.getupvalue",
-    "â debug.getupvalues",
-    "â debug.setconstant",
-    "â debug.setstack",
-    "â debug.setupvalue",
-    "â decompile",
-    "â delfile",
-    "â delfolder",
-    "â fireclickdetector",
-    "â fireproximityprompt",
-    "â firesignal",
-    "â firetouchinterest",
-    "â getcallbackvalue",
-    "â getcallingscript",
-    "â getconnections",
-    "â getcustomasset",
-    "â getgc",
-    "â getgenv_access",
-    "â gethiddenproperty",
-    "â gethui",
-    "â getinstances",
-    "â getloadedmodules",
-    "â getnamecallmethod",
-    "â getnilinstances",
-    "â getrawmetatable",
-    "â getrenderproperty",
-    "â getrenv",
-    "â getrunningscripts",
-    "â getscriptbytecode",
-    "â getscriptclosure",
-    "â getscripts",
-    "â getsenv",
-    "â getthreadidentity",
-    "â hookfunction",
-    "â hookmetamethod",
-    "â identifyexecutor",
-    "â iscclosure",
-    "â isexecutorclosure",
-    "â isfile",
-    "â isfolder",
-    "â islclosure",
-    "â isrbxactive",
-    "â isreadonly",
-    "â isrenderobj",
-    "â isscriptable",
-    "â listfiles",
-    "â loadfile",
-    "â loadstring",
-    "â lz4decompress",
-    "â lz4compress",
-    "â makefolder",
-    "â newcclosure",
-    "â readfile",
-    "â request",
-    "â restorefunction",
-    "â sethiddenproperty",
-    "â setrawmetatable",
-    "â setreadonly",
-    "â setrenderproperty",
-    "â setscriptable",
-    "â setthreadidentity",
-    "â writefile",
-    "\n",
-    "ðPassed the test with 100% success rate (87 out of 87)",
-    "â Total tests failed: 0",
-    "ð This test was made by senS (fuck all inco pastes, unc fakers)",
-    "Finished the test in " .. math.random(17.3, 38.2) .. " seconds",
-    "\n",
-    "sUNC, VERSION 1.51 n?Ã¿rn?z|v{nv{??qrÃ¶{v{Âvy",
-    "Contributors: vvultt, GRH, 0_void, Dottik, Pixeluted, bytevector(ð¥­), s.irius, citam."
-}
-
-for i, v in next, everything do
-    print(tostring(v))
-    task.wait()
-end
