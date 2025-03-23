951f4a232c091e225adfb90684f63a94ffb58755
Vxalware <jainasinth@gmail.com>
2025-03-23 18:24:23 +0800

    What the loader update

--- sUNC Faker/Loader.lua
+++ sUNC Faker/Loader.lua
@@ -1,1 +1,4 @@
+-- sUNC Faker
 loadstring(game:HttpGet("https://raw.githubusercontent.com/vxalware-bedwars-owner/Scripts-for-Roblox/refs/heads/main/sUNC%20Faker/MainScript.lua",true))()
+-- xUNC (BEST UNC)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/vxalware-bedwars-owner/Scripts-for-Roblox/refs/heads/main/sUNC%20Faker/xUNC.lua",true))()
