13fe89d3db0fb4793307670cad46db9ae614a0b3
Vxalware <jainasinth@gmail.com>
2025-03-24 18:41:24 +0800

    We had to shut sUNC faker down cause of the new 2.0 update :(

--- sUNC Faker/Loader.lua
+++
@@ -1,4 +0,0 @@
--- sUNC Faker
-loadstring(game:HttpGet("https://raw.githubusercontent.com/vxalware-bedwars-owner/Scripts-for-Roblox/refs/heads/main/sUNC%20Faker/MainScript.lua",true))()
--- xUNC (BEST UNC)
-loadstring(game:HttpGet("https://raw.githubusercontent.com/vxalware-bedwars-owner/Scripts-for-Roblox/refs/heads/main/sUNC%20Faker/xUNC.lua",true))()
