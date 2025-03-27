c7f03c21ec77df0b2ddf9105cb7a06a550ae65f7
Vxalware <jainasinth@gmail.com>
2025-03-27 19:13:21 +0800

    Night X Atlantis is finally OFFICIALLY BACK!1!

--- Night Modify/MainScript.lua
+++ Night Modify/MainScript.lua
@@ -1,23 +1,3 @@
---[[
--->> Adding CFrame Speed bypass, New method of inf fly, and a health display <<--
-
-local UserInputService = game:GetService("UserInputService")
-
-if UserInputService.TouchEnabled then
-    -- Night Extension for stuff
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/vxalware-bedwars-owner/Scripts-for-Roblox/refs/heads/main/Night%20Modify/Night%20Extension.lua"))()
-    -- Night Script loader
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/warprbx/NightRewrite/refs/heads/main/Night/Loader.luau"))()
-    -- Mobile Keyboard (you NEED this for Night to work on mobile 💀)
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/AZYsGithub/Delta-Scripts/refs/heads/main/MobileKeyboard.txt"))()
-else
-    -- Night Extension for stuff
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/vxalware-bedwars-owner/Scripts-for-Roblox/refs/heads/main/Night%20Modify/Night%20Extension.lua"))()
-    -- Night Script loader
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/warprbx/NightRewrite/refs/heads/main/Night/Loader.luau"))()
-end
-]]
-
 local Players = game:GetService("Players")
 local player = Players.LocalPlayer
 local UserInputService = game:GetService("UserInputService")
