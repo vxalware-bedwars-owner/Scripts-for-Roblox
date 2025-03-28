6e573767a30f7796cb04df3f0a6c2a5644045114
Vxalware <jainasinth@gmail.com>
2025-03-28 17:14:00 +0800

    AimAssist to BCC

--- BCC/MainScript.lua
+++ BCC/MainScript.lua
@@ -1,20 +1,48 @@
-local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/jensonhirst/Orion/main/source')))()
-local Window = OrionLib:MakeWindow({Name = "Bedwars Closet Cheat", HidePremium = false, SaveConfig = true, ConfigFolder = "BCC", IntroText = BCC 4 Bedwars})
-
-local Tab = Window:MakeTab({
-	Name = "Closet",
-	Icon = "rbxassetid://4483345998",
-	PremiumOnly = false
+local OrionLib = loadstring(game:HttpGet('https://raw.githubusercontent.com/jensonhirst/Orion/main/source'))()
+local Window = OrionLib:MakeWindow({
+    Name = "Bedwars Closet Cheat", 
+    HidePremium = false, 
+    SaveConfig = true, 
+    ConfigFolder = "BCC", 
+    IntroText = "BCC 4 Bedwars"
 })
 
-local Tab = Window:MakeTab({
-	Name = "Blatant",
-	Icon = "rbxassetid://4483345998",
-	PremiumOnly = false
+-- Closet Tab
+local ClosetTab = Window:MakeTab({
+    Name = "Closet",
+    Icon = "rbxassetid://4483345998",
+    PremiumOnly = false
 })
 
-local Tab = Window:MakeTab({
-	Name = "Features",
-	Icon = "rbxassetid://4483345998",
-	PremiumOnly = false
+ClosetTab:AddToggle({
+    Name = "AimAssist",
+    Default = false,
+    Callback = function(Value)
+        print("AimAssist toggled:", Value)
+        -- Add your AimAssist functionality here
+    end    
 })
+
+-- Blatant Tab
+local BlatantTab = Window:MakeTab({
+    Name = "Blatant",
+    Icon = "rbxassetid://4483345998",
+    PremiumOnly = false
+})
+
+-- Features Tab
+local FeaturesTab = Window:MakeTab({
+    Name = "Features",
+    Icon = "rbxassetid://4483345998",
+    PremiumOnly = false
+})
+
+OrionLib:MakeNotification({
+	Name = "BCC Notification",
+	Content = "BCC Has Injected Successfully! Have Fun!",
+	Image = "rbxassetid://4483345998",
+	Time = 5
+})
+
+-- Initialize the UI
+OrionLib:Init()
