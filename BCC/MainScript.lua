20cfbc391303d4a877b2f2cd9eede9dc1a187fc1
Vxalware <jainasinth@gmail.com>
2025-03-28 17:06:51 +0800

    Create MainScript.lua

---
+++ BCC/MainScript.lua
@@ -0,0 +1,20 @@
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/jensonhirst/Orion/main/source')))()
+local Window = OrionLib:MakeWindow({Name = "Bedwars Closet Cheat", HidePremium = false, SaveConfig = true, ConfigFolder = "BCC", IntroText = BCC 4 Bedwars})
+
+local Tab = Window:MakeTab({
+	Name = "Closet",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Blatant",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "Features",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
