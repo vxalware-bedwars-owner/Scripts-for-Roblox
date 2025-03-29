acdaaad4c40beb3470f2c32f91603fa100c3607a
Vxalware <jainasinth@gmail.com>
2025-03-29 14:02:15 +0800

    Temp Script Loader

--- BCC/Loader.lua
+++ BCC/Loader.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/vxalware-bedwars-owner/Scripts-for-Roblox/refs/heads/main/BCC/MainScript.lua",true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/vxalware-bedwars-owner/Scripts-for-Roblox/refs/heads/main/BCC/Temp%20Script.lua",true))()
