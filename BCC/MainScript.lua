c8e05c87edcdeef0aac3211bc90e95ab385cc398
Vxalware <jainasinth@gmail.com>
2025-03-30 14:15:22 +0800

    Update MainScript.lua

--- BCC/MainScript.lua
+++ BCC/MainScript.lua
@@ -7,59 +7,27 @@ local Window = OrionLib:MakeWindow({
     IntroText = "BCC 4 Bedwars"
 })
 
--- Closet Tab
-local ClosetTab = Window:MakeTab({
-    Name = "Closet",
-    Icon = "rbxassetid://4483345998",
-    PremiumOnly = false
+local Tab = Window:MakeTab({
+	Name = "Closet",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
 })
 
-ClosetTab:AddToggle({
-    Name = "AimAssist",
-    Default = false,
-    Callback = function(Value)
-        print("AimAssist toggled:", Value)
-        getgenv().InjectionCounter1 = getgenv().InjectionCounter1 or 0
-        getgenv().InjectionCounter1 = getgenv().InjectionCounter1 + 1
-        
-        if getgenv().InjectionCounter1 % 2 == 1 then
-            loadstring(game:HttpGet("https://raw.githubusercontent.com/vxalware-bedwars-owner/Scripts-for-Roblox/main/BCC/Others/AimAssist.lua", true))()
-        else
-            if loop then
-                loop:Disconnect()
-                loop = nil
-            end
-            
-            if FOVring then
-                FOVring:Remove()
-                FOVring = nil
-            end
-            
-            print("Aimbot script successfully uninjected.")
-        end
-    end    
+local Tab = Window:MakeTab({
+	Name = "Blatant",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
 })
 
--- Blatant Tab
-local BlatantTab = Window:MakeTab({
-    Name = "Blatant",
-    Icon = "rbxassetid://4483345998",
-    PremiumOnly = false
-})
-
--- Features Tab
-local FeaturesTab = Window:MakeTab({
-    Name = "Features",
-    Icon = "rbxassetid://4483345998",
-    PremiumOnly = false
+local Tab = Window:MakeTab({
+	Name = "Feature",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
 })
 
 OrionLib:MakeNotification({
-	Name = "BCC Notification",
-	Content = "BCC Has Injected Successfully! Have Fun!",
+	Name = "Injected",
+	Content = "Successfuly injected BCC! Have fun!",
 	Image = "rbxassetid://4483345998",
 	Time = 5
 })
-
--- Initialize the UI
-OrionLib:Init()
