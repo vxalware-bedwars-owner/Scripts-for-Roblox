5fbcf30a289aac5a0c05e31fce3ba64b8895bd91
Vxalware <jainasinth@gmail.com>
2025-04-08 13:02:25 +0800

    Update Complicated.lua

--- xUNC/.tests/Loadstrings/Complicated.lua
+++ xUNC/.tests/Loadstrings/Complicated.lua
@@ -1,4 +1,5 @@
 return function()
-    print("✅ Passed the Basic Loadsting test!")
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/acsu123/HOHO_H/main/Loading_UI'))()
+    print("✅ Passed the Complicated Loadsting test!")
     return true
 end
