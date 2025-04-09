e3d02b7082960bdf8de5dda9ee8aead59befce7b
Vxalware <jainasinth@gmail.com>
2025-04-09 10:53:14 +0800

    Update Basic.lua

--- xUNC/.tests/Loadstrings/Basic.lua
+++ xUNC/.tests/Loadstrings/Basic.lua
@@ -1,4 +1,10 @@
-return function()
-    print("✅ Passed the Basic Loadsting test!")
-    return true
+local passed, err = pcall(function()
+    -- Basic test: math operations
+    local result = 10 + 5
+end)
+
+if passed then
+    print("✅ Passed the basic loadstring test.")
+else
+    print("❌ Failed the basic loadstring test.")
 end
