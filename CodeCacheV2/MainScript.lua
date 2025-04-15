2379cd61e9f7ba1f571fd7fe212376ee9861124e
Vxalware <jainasinth@gmail.com>
2025-04-15 09:07:38 +0800

    Update MainScript.lua

--- CodeCacheV2/MainScript.lua
+++ CodeCacheV2/MainScript.lua
@@ -1,1 +1,89 @@
+local DiscordLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/discord"))()
+local win = DiscordLib:Window("CodeCacheV2 Re-made by SynthX, A Nerd, Vxalware, and sstvskids")
+local serv = win:Server("Game Scripts", "http://www.roblox.com/asset/?id=6031075938")
 
+local drops = serv:Channel("A Game Scripts")
+
+local drops = serv:Channel("B Game Scripts")
+
+local drop = drops:Dropdown("Blox Fruits 🍎", {"Redz Hub", "Speed Hub X", "Kncrypt Hub", "Alchemy Hub", "Ronix Hub"}, function(selectedOption)
+    
+    if selectedOption == "Redz Hub" then
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/realredz/BloxFruits/refs/heads/main/Source.lua"))()
+    elseif selectedOption == "Speed Hub X" then
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/Speed%20Hub%20X.lua"))()
+    elseif selectedOption == "Kncrypt Hub" then
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/3345-c-a-t-s-u-s/Kncrypt/refs/heads/main/sources/BloxFruit.lua"))()
+    elseif selectedOption == "Alchemy Hub" then
+        loadstring(game:HttpGet("https://scripts.alchemyhub.xyz"))()
+    elseif selectedOption == "Ronix Hub" then
+        loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/b5f968ca22436160479678e830766cc4.lua"))()
+    end
+    print("You selected: " .. selectedOption)
+end)
+
+local drop = drops:Dropdown("Bedwars 🛏️", {"Vape V4", "Night", "AlSploit", "Vxalware", "AuroraGPT", "Voidpaste"}, function(selectedOption)
+    
+    if selectedOption == "Cat V5" then
+        loadstring(game:HttpGet('https://raw.githubusercontent.com/new-qwertyui/CatV5/main/init.lua'))()
+    elseif selectedOption == "Night" then
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/vxalware-bedwars-owner/Scripts-for-Roblox/refs/heads/main/Night%20Modify/MainScript.lua"))()
+    elseif selectedOption == "AlSploit" then
+        loadstring(game:HttpGet("https://alsploit.bedwars.workers.dev/MainScript.lua?Key=yyy7eG1DjWIulkrfnzDrFP2No1H3si8YVODj93SqCd0jFh1CVk", true))()
+    elseif selectedOption == "Vxalware" then
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/vxalware-bedwars-owner/Vxalware-Rewrite/refs/heads/main/Vxware-Bedwars.lua"))() -- Credits to Vxalware
+    elseif selectedOption == "AuroraGPT" then
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/cocotv666/Aurora/main/Aurora_Loader"))()
+    elseif selectedOption == "Voidpaste" then
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/VapeVoidware/vapevoidware/main/NewMainScript.lua"))()
+    end
+    print("You selected: " .. selectedOption)
+end)
+
+local drops = serv:Channel("C Game Scripts")
+
+local drops = serv:Channel("D Game Scripts")
+
+local drops = serv:Channel("E Game Scripts")
+
+local drops = serv:Channel("F Game Scripts")
+
+local drops = serv:Channel("G Game Scripts")
+
+local drops = serv:Channel("H Game Scripts")
+
+local drops = serv:Channel("I Game Scripts")
+
+local drops = serv:Channel("J Game Scripts")
+
+local drops = serv:Channel("K Game Scripts")
+
+local drops = serv:Channel("L Game Scripts")
+
+local drops = serv:Channel("M Game Scripts")
+
+local drops = serv:Channel("N Game Scripts")
+
+local drops = serv:Channel("O Game Scripts")
+
+local drops = serv:Channel("P Game Scripts")
+
+local drops = serv:Channel("Q Game Scripts")
+
+local drops = serv:Channel("R Game Scripts")
+
+local drops = serv:Channel("S Game Scripts")
+
+local drops = serv:Channel("T Game Scripts")
+
+local drops = serv:Channel("U Game Scripts")
+
+local drops = serv:Channel("V Game Scripts")
+
+local drops = serv:Channel("W Game Scripts")
+
+local drops = serv:Channel("X Game Scripts")
+
+local drops = serv:Channel("Y Game Scripts")
+
+local drops = serv:Channel("Z Game Scripts")
