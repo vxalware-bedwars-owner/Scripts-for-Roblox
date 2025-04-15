0f72cd8155954dd0b526b07b3e405852a76f5e21
Vxalware <jainasinth@gmail.com>
2025-04-15 10:19:40 +0800

    Update Loader.lua

--- CodeCacheV2/Loader.lua
+++ CodeCacheV2/Loader.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/vxalware-bedwars-owner/Scripts-for-Roblox/refs/heads/main/CodeCacheV2.lua/NewMainScript.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/vxalware-bedwars-owner/Scripts-for-Roblox/refs/heads/main/CodeCacheV2/NewMainScript.lua"))()
