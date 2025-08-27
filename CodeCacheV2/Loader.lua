a90ef92f37eb4d86160380e3ac67c1fc95a028fa
Vxalware <jainasinth@gmail.com>
2025-08-27 16:27:15 +0800

    Currently has no updates or plans for any updates soon

--- CodeCacheV2/Loader.lua
+++ CodeCacheV2/Loader.lua
@@ -1,1 +1,2 @@
+-- Currently has no updates or plans for any updates soon
 loadstring(game:HttpGet("https://raw.githubusercontent.com/vxalware-bedwars-owner/Scripts-for-Roblox/refs/heads/main/CodeCacheV2/NewMainScript.lua"))()
