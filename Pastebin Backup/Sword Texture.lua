a93d4552084ebb0bc3a4dbfd22911ffa727fe540
Vxalware <jainasinth@gmail.com>
2025-09-30 21:53:25 +0800

    Update Sword Texture.lua

--- Pastebin Backup/Sword Texture.lua
+++ Pastebin Backup/Sword Texture.lua
@@ -1,10 +1,2 @@
-workspace.CurrentCamera.Viewmodel.ChildAdded:Connect(function(x)
-    if x and x:FindFirstChild("Handle") then
-        if string.find(x.Name:lower(), 'sword') then
-            x.Handle.Material = "ForceField"
-            x.Handle.MeshId = "rbxassetid://13471207377"
-            x.Handle.BrickColor = BrickColor.new("Hot pink")
-        end
-    end
-
-end)
+-- hem hem dont ask
+https://github.com/sstvskids/txtpacks
