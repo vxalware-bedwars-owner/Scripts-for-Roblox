c58a443a0e817395397bd44cca96a0513362369a
Vxalware <jainasinth@gmail.com>
2025-10-01 20:06:50 +0800

    slurp

--- Pastebin Backup/Sword Texture.lua
+++ Pastebin Backup/Sword Texture.lua
@@ -1,2 +1,12 @@
--- hem hem dont ask
+-- creds to stav 🙏
 https://github.com/sstvskids/txtpacks
+-- weird txt pack made by me
+workspace.CurrentCamera.Viewmodel.ChildAdded:Connect(function(x)
+    if x and x:FindFirstChild("Handle") then
+        if string.find(x.Name:lower(), 'sword') then
+            x.Handle.Material = "ForceField"
+            x.Handle.MeshId = "rbxassetid://13471207377"
+            x.Handle.BrickColor = BrickColor.new("Hot pink")
+        end
+    end
+end)
