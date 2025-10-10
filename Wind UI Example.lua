2864c80eb826ab31bdaa3913a588fe8c35611fbd
Vxalware <jainasinth@gmail.com>
2025-10-10 22:35:35 +0800

    Fix font weight

--- Wind UI Example.lua
+++ Wind UI Example.lua
@@ -64,7 +64,6 @@ local Tab1 = Window:Tab({
 local Section = Tab1:Section({ 
     Title = "Section",
     Box = false,
-    FontWeight = "SemiBold",
     TextTransparency = 0.05,
     TextXAlignment = "Left",
     TextSize = 17,
