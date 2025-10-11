0c6f07cbb423cd2aa25ffea1cfba4085ce267128
Vxalware <jainasinth@gmail.com>
2025-10-11 13:15:05 +0800

    testing.lol

---
+++ Best UI Libraries/Orion Lib Example.lua
@@ -0,0 +1,2 @@
+local OrionLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/thanhdat4461/GUI-Library/refs/heads/main/Orion%20Library.lua",true))()
+local Window = OrionLib:MakeWindow({Name = "Orion Example", HidePremium = false, SaveConfig = false, ConfigFolder = "OrionExample", IntroText = "Example UI by SynthX"})
