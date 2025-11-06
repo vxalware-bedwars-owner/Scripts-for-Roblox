f9d4562946f50a27fedbfd28f4a8e40b6fa36f9f
Vxalware <jainasinth@gmail.com>
2025-11-06 11:53:22 +0800

    Update Loader.lua

--- Discord UI Icon Skid/Loader.lua
+++ Discord UI Icon Skid/Loader.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/vxalware-bedwars-owner/Scripts-for-Roblox/refs/heads/main/Discord%20UI%20Icon%20Skid/NewMainScript.lua",true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/vxalware-bedwars-owner/Scripts/refs/heads/main/Discord%20UI%20Icon%20Skid/NewMainScript.lua",true))()
